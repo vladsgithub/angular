3864defe82fde261fe189aebf59c638d5af98f34
Uladzislau Stasevich <Uladzislau_Stasevich@epam.com>
2016-03-22 19:05:54 +0300

    dataService

--- ui/js/controllers.js
+++ ui/js/controllers.js
@@ -1,12 +1,21 @@
-myApp.run(function($rootScope){
-	$rootScope.alert = 'it is $rootScope.top';
-});
+//-----------------------------------------------------------------------------------------------------------------
+//Контроллер - координатор между представлением (view) и моделью (model), в этом случае логики в нем будет минимум.
+//Контроллеры не должны осуществлять манипуляции с DOM или содержать DOM селекторы
+//-----------------------------------------------------------------------------------------------------------------
 
-myApp.controller('parentController', function($scope) {
+//Контроллеры не должны осуществлять манипуляции с DOM или содержать DOM селекторы, для это существуют директивы.
+//	Точно также и бизнес-логика должна находиться в сервисах. Данные также должны храниться в сервисах
+//(за исключением случаев когда данные привязаны к $scope), поскольку сервисы, в отличие от контроллеров, это синглтоны,
+//	чье время жизни совпадает со временем жизни самого приложения. При разработке контроллеров лучше всего следовать
+//принципу единственной ответственности (SRP) и считать контроллер координатором между представлением и моделью,
+//	в этом случае логики в нем будет минимум.
+
+
+myApp.controller('parentController', ['$scope', function($scope) {
 	$scope.message = 'Parent controller message';
-});
+}]);
 
-myApp.controller('phoneController', function($scope, $rootScope) {
+myApp.controller('phoneController', ['$scope', '$rootScope', function($scope, $rootScope) {
 	$rootScope.fromController = 'A property of $rootScope is installed from controller';
 
 	$scope.phone = {
@@ -21,9 +30,9 @@ myApp.controller('phoneController', function($scope, $rootScope) {
 	$scope.items = [10, 20, 30, 40, 50];
 
 	this.text="Без использования $scope";
-});
+}]);
 
-myApp.controller('bindController', function($scope) {
+myApp.controller('bindController', ['$scope', function($scope) {
 	$scope.phones = [
 		{
 			name: 'Nokia Lumia 630',
@@ -55,9 +64,9 @@ myApp.controller('bindController', function($scope) {
 	];
 
 	$scope.htmlcode="Директива <b>ng-bind-html</b>";
-});
+}]);
 
-myApp.controller('modeController', function($scope) {
+myApp.controller('modeController', ['$scope', function($scope) {
 
 	$scope.phones = [{
 		name: 'Nokia Lumia 630',
@@ -105,9 +114,9 @@ myApp.controller('modeController', function($scope) {
 		value: 'Phones',
 		label: 'Смартфоны'
 	}];
-});
+}]);
 
-myApp.controller('styleController', function($scope) {
+myApp.controller('styleController', ['$scope', function($scope) {
 	$scope.somestyle = {
 		background: '#eee',
 		color: '#333'
@@ -116,9 +125,9 @@ myApp.controller('styleController', function($scope) {
 	$scope.data = {
 		visible: false
 	};
-});
+}]);
 
-myApp.controller('questionController', function($scope) {
+myApp.controller('questionController', ['$scope', function($scope) {
 	$scope.question = {
 		text: 'Какой js-фреймворк лучше использовать?',
 		author: 'Иван Иванов',
@@ -154,9 +163,9 @@ myApp.controller('questionController', function($scope) {
 	$scope.changeClass = function (e) {
 		$scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
 	}
-});
+}]);
 
-myApp.controller('customFilterController', function($scope) {
+myApp.controller('customFilterController', ['$scope', function($scope) {
 	$scope.question = {
 		text: 'Какой js-фреймворк лучше использовать?',
 		author: 'Иван Иванов',
@@ -186,14 +195,37 @@ myApp.controller('customFilterController', function($scope) {
 	$scope.voteDown = function (answer){
 		answer.rate--;
 	};
-});
+}]);
 
-myApp.controller('validationController', function($scope) {
-	$scope.save = function (answer, answerForm){
+myApp.controller('validationController', ['$scope', function($scope) {
+	$scope.save = function (answer, answerForm) {
 		// С помощью выражения answerForm.$valid просматриваем, прошла форма валидацию или нет.
 		if(answerForm.$valid){
 			// действия по сохранению
 			alert(answer.author + ", ваш ответ сохранен");
 		}
 	};
-});
+}]);
+
+myApp.controller('dataServiceController', ['$scope', 'dataService', function($scope, dataService) {
+	// dataService - под таким названием был создан сервис (в файле services.js), который обеспечивает этот контроллер данными
+	$scope.question = dataService.question;
+
+	$scope.voteUp = function (answer){
+		answer.rate++;
+	};
+	$scope.voteDown = function (answer){
+		answer.rate--;
+	};
+}]);
+
+myApp.controller('moduleServiceController', ['$scope', 'questionService', function ($scope, questionService){
+	$scope.question = questionService.question;
+
+	$scope.voteUp = function (answer){
+		answer.rate++;
+	};
+	$scope.voteDown = function (answer){
+		answer.rate--;
+	};
+}]);
--- ui/js/directives.js
+++ ui/js/directives.js
@@ -1,9 +1,13 @@
+//-----------------------------------------------------------------------------------------------
+//Директивы должны осуществлять манипуляции с DOM или содержать DOM селекторы.
+//-----------------------------------------------------------------------------------------------
+
 //В качестве названия директивы используется "answerList". Согласно соглашениям об именовании любая буква
 // в верхнем регистре расциенивается как начало нового слова, которое будет отделено от предыдущего дефисом.
 //То есть для AngularJS "answerList" будет представлять "answer-list" или "answer-List"
 
 myApp.directive("answerList", function () {
-	//В функцию создания директивы передается три параметра:
+	//В функцию создания директивы передается три параметра именно в такой последовательности:
 	//scope - объект $scope, установленный в контроллере
 	//element - элемент, к которому применяется директива
 	//attrs - объект атрибутов в виде {название_атрибута: его_значение, ...}
@@ -29,5 +33,115 @@ myApp.directive("answerList", function () {
 				ulElem.append(liElem);
 			}
 		}
+	}
+});
+
+myApp.directive("answerListParam", function () {
+	return {
+		//свойство restrict позволяет указать объект, к которому директива будет применяться:
+		//E: директива применяется к элементу:
+		// <answer-list-param source="question" />
+		//A: директива применяется к атрибуту - в предыдущей директиве answerList как раз применялся такой подход, но без использования свойства restrict:
+		// <div answer-list-param="question"></div>
+		//C: директива применяется к классу: (не рекомендуется использовать)
+		// <div class="answer-list-param"></div>
+		//M: директива применяется к комментарию: (не рекомендуется использовать)
+		// <!-- directive: answer-list-param -->
+		restrict: "EACM",
+
+		//Свойство link позволяет задать функцию директивы
+		link: function (scope, element, attrs) {
+
+			//var data = scope[attrs["answerList"] || attrs["source"]]; // если предусматривать возможность получения из двух атрибутов
+			var data = scope[attrs["source"]];
+
+			if (angular.isArray(data.answers)) {
+				var divElem = angular.element("<div>").addClass("answers");
+
+				if (element[0].nodeName == "#comment") {
+					element.parent().append(divElem);
+				} else {
+					element.append(divElem);
+				}
+
+				for (var i = 0; i < data.answers.length; i++) {
+					var answerElem = angular.element('<div>').addClass("answer");
+
+					answerElem.append(angular.element('<b>').text(data.answers[i].text));
+					answerElem.append(angular.element('<p>').text(data.answers[i].author));
+
+					var iElem =angular.element('<i>').text(data.answers[i].date);
+					var dateElem = angular.element('<p>').append(iElem);
+
+					answerElem.append(dateElem);
+					divElem.append(answerElem);
+				}
+			}
+		}
+	}
+});
+
+myApp.directive("answerListTemplate", function () {
+	return {
+		restrict: "A",
+		link: function (scope, element, attrs) {
+			scope.data = scope[attrs["answerListTemplate"]];
+		},
+		template: "<div class='answers'>" +
+		"<div ng-repeat='answer in data.answers' class='answer'>" +
+		"<div class='vote'>" +
+		"<a class='vote-up' ng-click='voteUp(answer)'></a>" +
+		"<span class='rate'>{{answer.rate}}</span>" +
+		"<a class='vote-down' ng-click='voteDown(answer)'></a>" +
+		"</div>" +
+		"<b>{{answer.text | formatText}}</b>" +
+		"<p>{{answer.author}}</p>" +
+		"<p><i>{{answer.date}}</i></p>" +
+		"</div></div>"
+	}
+});
+
+myApp.directive("answerListUrlTemplate", function () {
+	return {
+		restrict: "A",
+		templateUrl: "templates/answerListUrlTemplate.html"
+	}
+});
+
+myApp.directive("answerListTransclude", function () {
+	return {
+		restrict: "A",
+		templateUrl: "templates/answerListUrlTransclude.html",
+		transclude: true
+	}
+});
+
+myApp.directive("answerListCompile", function($compile) {
+	//сервис $compile преобразует HTML-код, содержащий выражения привязки и директивы,
+	//в функцию, вызываемую для генерации контента.
+	//Сервис $compile представляет функцию, генерирующую контент из фрагментов html.
+	//Чтобы наполнить контент данными из контроллера, в функцию передается параметр scope: compileFn(scope);
+	//Завершает работу метод element.append(answersElem), который добавляет сгенерированный контент в структру DOM веб-страницы.
+
+	//В каких случаях удобно использовать в директиве сервис $compile:
+	//	когда надо преобразовать имеющийся HTML-код в шаблон и объединить этот шаблон с объектом scope для последующего использования
+
+	return function (scope, element, attrs) {
+		var content = "<div class='answers'>" +
+			"<div ng-repeat='answer in question.answers' class='answer'>" +
+			"<div class='vote'>" +
+			"<a class='vote-up' ng-click='voteUp(answer)'></a>" +
+			"<span class='rate'>{{answer.rate}}</span>" +
+			"<a class='vote-down' ng-click='voteDown(answer)'></a>" +
+			"</div>" +
+			"<b>{{answer.text | formatText}}</b>" +
+			"<p>{{answer.author}}</p>" +
+			"<p><i>{{answer.date}}</i></p>" +
+			"</div></div>";
+		var answersElem = angular.element(content);
+		var compileFn = $compile(answersElem);
+
+		compileFn(scope);
+		element.append(answersElem);
 	}
 });
